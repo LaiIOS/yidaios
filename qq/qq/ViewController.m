c6ef2d79e0acef5e623f1b0e8b84159387585391
LaiIOS <1429081958@qq.com>
2016-11-05 16:26:25 +0800

    刷新了一下

--- qq/qq/ViewController.m
+++ qq/qq/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //刷新
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
